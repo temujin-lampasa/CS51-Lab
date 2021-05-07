ee3e89a6989860a229aa83d43d1b3457d44b86ad
temujin-lampasa <temujin.lampasa@obf.ateneo.edu>
2021-05-07 19:02:27 +0800

    Add Lab4

---
+++ Lab4/lab4.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int multiply(int a, int b){
+    int ret = 0;
+    bool b_is_neg = b < 0;
+
+    if (b_is_neg) {
+        b = ~b + 1;
+    }
+    
+    while (b != 0){
+        if (b % 2 == 1){
+            ret += a;
+        }
+        a <<= 1;
+        b >>= 1;
+    }
+
+    if (b_is_neg){
+        ret = ~ret + 1;
+    }
+    return ret;
+}
+
+int main(){
+    
+    // Test
+    int a_test[]   = {2, 2, 0, -2,  0, -2,  2, 2, 1, -2,  1};
+    int b_test[]   = {2, 0, 2,  0, -2,  2, -2, 1, 2,  1, -2};
+    try{
+        for (unsigned long int i=0; i<sizeof(a_test)/sizeof(a_test[0]); i++){
+            int a = a_test[i];
+            int b = b_test[i];
+            int ret = multiply(a, b);
+            bool pass = (ret == a * b);
+            // https://stackoverflow.com/questions/667183/padding-stl-strings-in-c
+            cout << setw(2) << a << " * " << setw(2) << b << " = " << setw(2) << ret << endl;
+            if (multiply(a, b) != a*b) {
+                throw exception();
+            }
+        }
+    } catch (exception e) {
+        cout << "Error" <<  endl;
+    }
+    return 0;
+}
